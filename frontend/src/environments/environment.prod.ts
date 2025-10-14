880ec323e5cb5b9fc9f85349653bcf7e4d9d0f00
rltver <rafaluquetrujillo@gmail.com>
2025-10-14 12:43:24 +0200

    environment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// environment.prod.ts (producción)
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:3000/',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// environment.prod.ts (producción)
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/',
+};
